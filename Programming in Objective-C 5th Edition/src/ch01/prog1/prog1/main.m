5065b81e2a212205a0375ac56382fd4289843015
FengHua <frankfenghua@gmail.com>
2013-08-16 23:35:54 -0400

    hello world

---
+++ Programming in Objective-C 5th Edition/src/ch01/prog1/prog1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  prog1
+//
+//  Created by fenghua on 2013-08-14.
+//  Copyright (c) 2013 ClassroomM. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
