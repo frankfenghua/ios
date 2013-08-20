ded8c10a8a7e88d5e0f693f2c8969db40c93b49d
Feng Hua <fhua@caesars.com>
2013-08-20 18:13:35 -0400

    impl BIDDependentComponentPickerViewController

--- Begning IOS 6 Developement/code/07 - MyPickers/MyPickers/MyPickers/BIDDependentComponentPickerViewController.h
+++ Begning IOS 6 Developement/code/07 - MyPickers/MyPickers/MyPickers/BIDDependentComponentPickerViewController.h
@@ -7,7 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#define kStateComponent 0
+#define kZipComponent   1
 
-@interface BIDDependentComponentPickerViewController : UIViewController
+@interface BIDDependentComponentPickerViewController : UIViewController<UIPickerViewDelegate, UIPickerViewDataSource>
+@property (strong, nonatomic) IBOutlet UIPickerView *dependentPicker;
+@property (strong, nonatomic) NSDictionary *stateZips;
+@property (strong, nonatomic) NSArray *states;
+@property (strong, nonatomic) NSArray *zips;
+- (IBAction) buttonPressed;
 
 @end
--- Begning IOS 6 Developement/code/07 - MyPickers/MyPickers/MyPickers/BIDDependentComponentPickerViewController.m
+++ Begning IOS 6 Developement/code/07 - MyPickers/MyPickers/MyPickers/BIDDependentComponentPickerViewController.m
@@ -27,12 +27,85 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    NSBundle *bundle = [NSBundle mainBundle];
+    NSURL *plistURL = [bundle URLForResource:@"statedictionary"
+                               withExtension:@"plist"];
+    self.stateZips = [NSDictionary
+                      dictionaryWithContentsOfURL:plistURL];
+    
+    NSArray *allStates = [self.stateZips allKeys];
+    NSArray *sortedStates = [allStates sortedArrayUsingSelector:
+                             @selector(compare:)];
+    self.states = sortedStates;
+    NSString *selectedState = self.states[0];
+    self.zips = self.stateZips[selectedState];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)buttonPressed
+{
+    NSInteger stateRow = [self.dependentPicker
+                          selectedRowInComponent:kStateComponent];
+    NSInteger zipRow = [self.dependentPicker
+                        selectedRowInComponent:kZipComponent];
+    NSString *state = self.states[stateRow];
+    NSString *zip = self.zips[zipRow];
+    NSString *title = [[NSString alloc] initWithFormat:
+                       @"You selected zip code %@.", zip];
+    NSString *message = [[NSString alloc] initWithFormat:
+                         @"%@ is in %@", zip, state];
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title
+                                                    message:message
+                                                   delegate:nil
+                                          cancelButtonTitle:@"OK"
+                                          otherButtonTitles:nil];
+    [alert show];
+}
+
+#pragma mark -
+#pragma mark Picker Data Source Methods
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 2;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView
+numberOfRowsInComponent:(NSInteger)component {
+    if (component == kStateComponent) {
+        return [self.states count];
+    } else {
+        return [self.zips count];
+    }
+}
+
+#pragma mark Picker Delegate Methods
+- (NSString *)pickerView:(UIPickerView *)pickerView
+             titleForRow:(NSInteger)row
+            forComponent:(NSInteger)component
+{
+    if (component == kStateComponent) {
+        return self.states[row];
+    } else {
+        return self.zips[row];
+    }
+}
+- (void)pickerView:(UIPickerView *)pickerView
+      didSelectRow:(NSInteger)row
+       inComponent:(NSInteger)component
+{
+    if (component == kStateComponent) {
+        NSString *selectedState = self.states[row];
+        self.zips = self.stateZips[selectedState];
+        [self.dependentPicker reloadComponent:kZipComponent];
+        [self.dependentPicker selectRow:0
+                            inComponent:kZipComponent
+                               animated:YES];
+         }
+         
 }
 
 @end
