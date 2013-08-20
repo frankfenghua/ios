1656590ee2dbe2dc281aee7e504682c45414761c
Feng Hua <fhua@caesars.com>
2013-08-20 17:57:16 -0400

    impl BIDDoubleComponentPickerViewController

--- Begning IOS 6 Developement/code/07 - MyPickers/MyPickers/MyPickers/BIDDoubleComponentPickerViewController.h
+++ Begning IOS 6 Developement/code/07 - MyPickers/MyPickers/MyPickers/BIDDoubleComponentPickerViewController.h
@@ -8,6 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
-@interface BIDDoubleComponentPickerViewController : UIViewController
+#define kFillingComponent 0
+#define kBreadComponent   1
 
+@interface BIDDoubleComponentPickerViewController : UIViewController<UIPickerViewDelegate, UIPickerViewDataSource>
+@property (strong, nonatomic) IBOutlet UIPickerView *doublePicker;
+@property (strong, nonatomic) NSArray *fillingTypes;
+@property (strong, nonatomic) NSArray *breadTypes;
+-(IBAction)buttonPressed;
 @end
