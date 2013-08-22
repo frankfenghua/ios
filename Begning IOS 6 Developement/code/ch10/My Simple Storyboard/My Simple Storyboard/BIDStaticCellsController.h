66ddc24e39d04eeaaf57ae699a6fc6ef476be887
Feng Hua <fhua@caesars.com>
2013-08-22 12:15:35 -0400

    my Simple StoryBoard sample- part 2, static cells

---
+++ Begning IOS 6 Developement/code/ch10/My Simple Storyboard/My Simple Storyboard/BIDStaticCellsController.h
@@ -0,0 +1,15 @@
+//
+//  BIDStaticCellsController.h
+//  My Simple Storyboard
+//
+//  Created by fenghua on 2013-08-22.
+//  Copyright (c) 2013 fenghua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDStaticCellsController : UITableViewController
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+
+@end
