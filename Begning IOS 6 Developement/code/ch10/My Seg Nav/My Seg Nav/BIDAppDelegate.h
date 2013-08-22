f1559ae934faf26382d0c7593b511285e8f53570
Feng Hua <fhua@caesars.com>
2013-08-22 17:01:42 -0400

    add my Segue Nav sample, part 1

---
+++ Begning IOS 6 Developement/code/ch10/My Seg Nav/My Seg Nav/BIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIDAppDelegate.h
+//  My Seg Nav
+//
+//  Created by fenghua on 2013-08-22.
+//  Copyright (c) 2013 fenghua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Begning IOS 6 Developement/code/ch10/My Seg Nav/My Seg Nav/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  My Seg Nav
+//
+//  Created by fenghua on 2013-08-22.
+//  Copyright (c) 2013 fenghua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BIDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BIDAppDelegate class]));
+    }
+}
